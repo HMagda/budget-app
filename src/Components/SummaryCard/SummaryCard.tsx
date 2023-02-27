2f1babfd46b553d978c2536ca5bcae290328601e
Magdalena <magdalenahyra@wp.pl>
2023-02-27 20:07:29 +0100

    Edited SummaryCard component and button styles in SummaryCard, DoughnutChart, and LineChart

--- src/Components/SummaryCard/SummaryCard.tsx
+++ src/Components/SummaryCard/SummaryCard.tsx
@@ -1,15 +1,92 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import './SummaryCard.modules.scss';
 
+type MonthlyExpense = {
+  sumExpense: number;
+  month: string;
+  id: number;
+  year: number;
+};
+
+type MonthlyIncome = {
+  sumIncome: number;
+  month: string;
+  id: number;
+  year: number;
+};
+
+const currentYear = new Date().getFullYear();
+
 const SummaryCard: React.FC = () => {
+  const [selectedYear, setSelectedYear] = useState<number>(currentYear);
+  const [monthlyExpenses, setMonthlyExpenses] = useState<MonthlyExpense[]>([]);
+  const [monthlyIncome, setMonthlyIncome] = useState<MonthlyIncome[]>([]);
+  const [allYearsArr, setAllYearsArr] = useState<number[]>([]);
+
+  const fetchMonthlyData = (
+    endpoint: string,
+    setter: React.Dispatch<React.SetStateAction<any[]>>
+  ) => {
+    fetch(`http://localhost:5000/${endpoint}`)
+      .then((res) => res.json())
+      .then((data: any[]) => {
+        setter(data);
+        const years = data.map((item) => item.year);
+        const uniqueYears = Array.from(new Set(years));
+        setAllYearsArr(uniqueYears);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    fetchMonthlyData('monthly-expense', setMonthlyExpenses);
+    fetchMonthlyData('monthly-income', setMonthlyIncome);
+  }, []);
+
+  const handleYearChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedYear(parseInt(event.target.value));
+  };
+
+  const calculateSum = (items: any[], key: string) => {
+    const filteredItems = items.filter((item) => item.year === selectedYear);
+    if (filteredItems.length === 0) {
+      return 0;
+    }
+    const sum = filteredItems.reduce(
+      (accumulator, current) => accumulator + current[key],
+      0
+    );
+    return sum;
+  };
+
   return (
     <div className='summary-card'>
-      <h1>Total balance in {new Date().getFullYear()}</h1>
-
+      <h1>
+        Total balance in &nbsp;
+        <select
+          id='year-select'
+          value={selectedYear}
+          onChange={handleYearChange}
+          className='year-select'
+        >
+          {allYearsArr &&
+            allYearsArr.map((year, i) => {
+              return (
+                <option key={i} value={year} className='year-option'>
+                  {year}
+                </option>
+              );
+            })}
+        </select>
+      </h1>
       <div className='detail-info'>
         <h2>Income</h2>
         <h2>Expense</h2>
+      </div>
+      <div className='detail-info'>
+        <h2>${calculateSum(monthlyIncome, 'sumIncome')}</h2>
+        <h2>${calculateSum(monthlyExpenses, 'sumExpense')}</h2>
       </div>
     </div>
   );
